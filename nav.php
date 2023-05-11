dd9c617c583d781df4421f3b3f55e6b07f3ade95
bilal91rz <112877274+Bilal200614@users.noreply.github.com>
2023-05-11 10:06:45 +0200

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,22 @@
+<header>
+	<nav>
+  <img id="hond" src="img/BARAlogo.png"></img>
+  <div class="kat">
+    <ul>
+      <li><a href="home.php">Home</a></li>
+      <li><a href="shop.php">Shop</a></li>
+      <li class="dropdown">
+        	<a href="#" class="dropbtn">Over ons</a>
+        	<div class="dropdown-content">
+          	<a href="filter.php">Abdel</a>
+          	<a href="index.php">Ali</a>
+          	<a href="Azfar.php">Azfar</a>
+		  	<a href="Bilal.php">Bilal</a>
+		  	<a href="Fraishel.php">Fraishel</a>
+        	</div>
+      	</li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </div>
+</nav>
+	</header>
